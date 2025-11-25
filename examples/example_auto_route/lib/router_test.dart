227a7c3d96a0b566b002afab6ad7ba4e05a95f9d
iliya <iliyazelenkog@gmail.com>
2025-11-25 02:07:32 +0200

    chore: update pubspec.lock files and remove unused router_test.dart

--- examples/example_auto_route/lib/router_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-part 'router_test.gr.dart';
-
-@AutoRouterConfig()
-class MyRouter extends _$MyRouter {
-  @override
-  List<AutoRoute> get routes => [];
-}
